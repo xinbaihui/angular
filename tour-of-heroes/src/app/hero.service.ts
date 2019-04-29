4d3a1067d69e4d50b1bd217232da614de3e077d9
Xin <Ella.Xin@vce.com>
2019-04-29 10:45:39 +0800

    add function: add hero; delete hero

--- tour-of-heroes/src/app/hero.service.ts
+++ tour-of-heroes/src/app/hero.service.ts
@@ -11,6 +11,9 @@ import { MessageService } from './message.service'
 })
 export class HeroService {
   private heroesUrl = 'api/heroes' // URL to web api
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  }
 
   constructor(
     private http: HttpClient,
@@ -39,13 +42,31 @@ export class HeroService {
   }
 
   updateHero(hero: Hero): Observable<any> {
-    const httpOptions = {
-      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-    }
-    return this.http.put(this.heroesUrl, hero, httpOptions)
+    return this.http.put(this.heroesUrl, hero, this.httpOptions)
       .pipe(
         tap(_ => this.log(`updated hero id=${hero.id}`)),
         catchError(this.handleError<any>('updateHero'))
+      )
+  }
+
+  /** POST: add a new hero to the server */
+  addHero (hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions)
+      .pipe(
+        tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+        catchError(this.handleError<Hero>('addHero'))
+      )
+  }
+
+  /** DELETE: delete the hero from the server */
+  deleteHero (hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions)
+      .pipe(
+        tap(_ => this.log(`deleted hero id=${id}`)),
+        catchError(this.handleError<Hero>('deleteHero'))
       )
   }
 
--- tour-of-heroes/src/app/heros/heros.component.ts
+++ tour-of-heroes/src/app/heros/heros.component.ts
@@ -20,4 +20,18 @@ export class HerosComponent implements OnInit {
   getHeros() : void {
     this.heroService.getHeroes().subscribe(heros => this.heros = heros)
   }
+
+  add(name: string): void {
+    name = name.trim()
+    if (!name) {
+      return
+    }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => this.heros.push(hero))
+  }
+
+  delete(hero: Hero): void {
+    this.heros = this.heros.filter(h => h !== hero)
+    this.heroService.deleteHero(hero).subscribe()
+  }
 }
