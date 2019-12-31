419fa064da6b4ece4129691d9f52410e063ee814
Xin <Ella.Xin@vce.com>
2019-12-31 11:42:55 +0800

    add routes

--- router-demo/src/app/app-routing.module.ts
+++ router-demo/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'crisis',
+  component: CrisisListComponent
+}, {
+  path: 'heros',
+  component: HeroListComponent
+}, {
+  path: '',
+  pathMatch: 'full',  // 告诉路由器如何用URL匹配路由的路径， full：URL 中剩下的、未匹配的部分必须等于 ''
+                      // 'prefix'：当剩下的URL 以这个跳转路由中的 prefix 值开头时，就会匹配上这个跳转路由。
+  redirectTo: 'heros'
+}, {
+  path: '**',
+  component: PageNotFoundComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
